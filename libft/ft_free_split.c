6b39fc89cc1bd1425d80a06ad2470551f6ee228b
fbruggem <fbruggem@student.42heilbronn.de>
2022-07-19 16:19:33 +0200

    Updated Free_split

--- libft/ft_free_split.c
+++ libft/ft_free_split.c
@@ -6,7 +6,7 @@
 /*   By: fbruggem <fbruggem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 09:53:03 by fbruggem          #+#    #+#             */
-/*   Updated: 2022/06/09 10:02:19 by fbruggem         ###   ########.fr       */
+/*   Updated: 2022/07/19 16:17:49 by fbruggem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_free_split(char **split)
 	int	i;
 
 	i = 0;
+	if (!split)
+		return ;
 	while (split[i])
 		free(split[i++]);
 	free(split);
